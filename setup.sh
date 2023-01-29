70014d507fee78083a3d045d051c92358e28e497
Nuno Souto <nsouto@lostlink.net>
2023-01-29 10:36:39 -0500

    Update Setup Script
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd /home/ubuntu
 # disable the restart dialogue and install several packages
 sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 sudo apt-get update
-sudo apt install wget git python3 python3-venv build-essential net-tools awscli -y
+sudo apt install wget git python3 python3-pip python3-venv build-essential net-tools awscli supervisor -y
 
 # install CUDA (from https://developer.nvidia.com/cuda-downloads)
 wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
@@ -51,6 +51,8 @@ mv stable-diffusion-2-1-base/v2-1_512-ema-pruned.ckpt stable-diffusion-webui/mod
 rm -rf stable-diffusion-2-1-base/
 wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml
 cp v2-inference.yaml stable-diffusion-webui/models/Stable-diffusion/v2-1_512-ema-pruned.yaml
+
+# More Models ...
 
 ## Finish and start StableDiffusion
 # change ownership of the web UI so that a regular user can start the server
